78f7c6c24723bf7a173044a0044ae563d4ab60db
Victor Li <gwli@outlook.com>
2019-04-24 14:12:16 +0800

    change the log name

--- devtools/run_sample.sh
+++ devtools/run_sample.sh
@@ -5,7 +5,7 @@ cl=`git rev-parse HEAD|cut -c1-7`
 cd $TOP
 
 rm -fr *.log
-python mountain_climb_run.py 2>&1 |tee TPA_LSTM_${cl}_$timestamp.log
-python pole_run.py  2>&1 |tee test_$timestamp.log
-python xor_run.py  2>&1 |tee test_$timestamp.log
+python mountain_climb_run.py 2>&1 |tee mountain_climb_run_${cl}_$timestamp.log
+python pole_run.py  2>&1 |tee pole_run_$timestamp.log
+python xor_run.py  2>&1 |tee x_or_run_$timestamp.log
 
